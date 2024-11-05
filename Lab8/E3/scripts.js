dd735705f4558a2ca6c956c6a254e174fee39735
3m1ly311y <cf23xn@brocku.ca>
2024-11-05 13:48:14 -0500

    cx

--- Lab8/E3/scripts.js
+++ Lab8/E3/scripts.js
@@ -1,8 +1,8 @@
 //This will output when the page loads
-
-document.Write("Output Zero Here");
+document.write("Output Zero Here");
 
 //This will output when a button is pressed
-function theRest(){
+function therest(){
 	document.ElementById("output3").innerHTML="Output Three Here";
 }
+
