758ff8a54dd322ec4b685dec28c36c95007ec07e
3m1ly311y <cf23xn@brocku.ca>
2024-11-05 11:57:13 -0500

    b

--- Lab8/scripts3.js
+++ Lab8/scripts3.js
@@ -1,8 +1,8 @@
 //This will output when the page loads
-document.write("Output Zero Here");
-
+function therest(){
+document.ElementById("Output Zero Here").innerHTML="Output Zero Here";
+}
 //This will output when a button is pressed
 function therest(){
 	document.ElementById("output3").innerHTML="Output Three Here";
 }
-
