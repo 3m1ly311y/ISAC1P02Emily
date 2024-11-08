c67a55b71530d6a1f03a3de89fdb5cd62af67f3a
3m1ly311y <cf23xn@brocku.ca>
2024-11-08 13:30:21 -0500

    Update scripts.js

--- Lab8/E3/scripts.js
+++ Lab8/E3/scripts.js
@@ -3,5 +3,5 @@ document.write("<p>Output Zero Here</p>");
 
 //This will output when a button is pressed
 function therest(){
-	document.ElementbyId("output3").innerHTML="Output Three Here";
+	document.getElementbyId("output3").innerHTML="Output Three Here";
 }
