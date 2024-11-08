868827b37554d75450c794f6dd929a7d50d3d103
3m1ly311y <cf23xn@brocku.ca>
2024-11-08 12:24:24 -0500

    d

--- Lab8/E2/scripts.js
+++ Lab8/E2/scripts.js
@@ -1,5 +1,5 @@
 //Sets the initial colours of the three elements on page load
-document.getElementById("blue").style.color="blue";
+document.getElementById("one").style.color="blue";
 document.getElementById("green").style.color="green";
 document.getElementById("red").style.color="red";
 
@@ -10,7 +10,7 @@ function mix(){
 	var second = "BB9320";
 	var third = "#AB00FF";
 
-	document.getElementById("blue").style.color=first;
+	document.getElementById("one").style.color=first;
 	document.getElementBy("green").style.colour=second;
 	document.getElementById("red").style.color=third;
 
