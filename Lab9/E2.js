534c76763337b49fb329b479056a357830f7d1ec
3m1ly311y <cf23xn@brocku.ca>
2024-11-17 15:46:22 -0500

    Update E2.js

--- Lab9/E2.js
+++ Lab9/E2.js
@@ -1,3 +1,5 @@
 var prompt = prompt("What is your age?");
 console.log(prompt);
 var todayYear = getFullDate.getFullYear();
+var birthYear = getFullDate - prompt;
+var outPut = document.getElementsById("yearHere").innerHTML = birthYear;
