e911f1058921c4df06ab55201bcdc92022d54d2b
3m1ly311y <cf23xn@brocku.ca>
2024-11-17 15:34:47 -0500

    f

--- Lab9/E3.js
+++ Lab9/E3.js
@@ -1,3 +1,3 @@
 function addNumbers(){
-  document.getElementById("Output").innerHTML = Number 1 + "/" + Number 2;
-}
+var addNumb = number1.getElementById("number1") + number2.getElementById("number2");
+document.getElementsById("Output").innerHTML = addNumb;
