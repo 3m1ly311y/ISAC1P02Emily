75c507fa54207f1fb3faca2dce49757ad65a660d
3m1ly311y <cf23xn@brocku.ca>
2024-11-17 14:55:10 -0500

    Update E1.js

--- Lab9/E1.js
+++ Lab9/E1.js
@@ -1,11 +1,11 @@
-  var start = new Date()
-  var start = start.getTime();
+var start = new Date();
+var start = start.getTime();
 
 function stopTime(){
-  var stop = new Date()
-  var stop = stop.getTime();
+var stop = new Date();
+var stop = stop.getTime();
 
-  var elapsed = stop.getTime() - start.getTime();
+var elapsed = stop.getTime() - start.getTime();
 
-  var popUP = alert("You have been on this page for: " + elapsed + " seconds");
+var popUP = alert("You have been on this page for: " + elapsed + " seconds");
 }
